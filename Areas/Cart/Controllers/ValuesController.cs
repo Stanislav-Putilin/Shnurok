1f7e694966ed74538989f5ecf8c06ecf15368584
gingheea <alexstelmakhanatol@gmail.com>
2025-01-14 16:15:04 +0100

    Добавив контролер

---
+++ Areas/Cart/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace shnurok.Areas.Cart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
